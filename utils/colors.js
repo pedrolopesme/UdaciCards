2b83fc6886e6bb4c918b3b7baec106cb6368e6ba
Pedro Mendes <pedrolopesme@gmail.com>
2019-01-23 22:54:07 -0200

    minor: add colors util lib

---
+++ utils/colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+    Black: '#262626',
+    Blue: '#3c40c6',
+    Gray: '#757575',
+    Green: '#05c46b',
+    Orange: '#ffa801',
+    Purple: '#472a79',
+    DarkPurple: '#2d1c4e',
+    Pink: '#f53b57',
+    Red: '#ff3f34',
+    White: '#ffffff',
+    Yellow: '#ffbf00',
+}
+
+export default Colors;

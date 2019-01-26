3b0a0853604129697ee16ce1df6c241f14b6e1ac
Pedro Mendes <pedrolopesme@gmail.com>
2019-01-26 18:45:40 -0200

    feature: add new deck

---
+++ components/NewDeck.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, AsyncStorage } from 'react-native';
+import uuid from 'uuid';
+import Colors from '@utils/colors'
+
+class NewDeck extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { id: uuid(), deckTitle: '' };
+  }
+
+  saveDeck = () => {
+    if (!this.validate()) {
+      return;
+    }
+
+    const deck = {
+      id: this.state.id,
+      key: this.state.id,
+      title: this.state.deckTitle,
+      questions: [],
+    }
+    AsyncStorage.setItem(this.state.id, JSON.stringify(deck));
+    this.props.navigation.navigate('ShowDeck', {
+      home: true,
+      deckId: this.state.id,
+      deckName: deck.title,
+      questions: deck.questions.length,
+    }) && this.resetState();
+  }
+
+  validate = () => {
+    if (this.state.deckTitle === '') {
+      alert('Hey, you can\'t add a new deck without a name. 😩');
+      return false;
+    }
+    return true;
+  }
+
+  resetState = () =>
+    this.setState({ id: uuid(), deckTitle: '' });
+
+  render = () => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          What is the title of your new deck?
+        </Text>
+        <TextInput
+          placeholder='Deck Title'
+          onChangeText={deckTitle => this.setState({ deckTitle })}
+          style={styles.input}
+          value={this.state.deckTitle} />
+        <TouchableOpacity
+          onPress={this.saveDeck}
+          style={styles.buttonTouchable}>
+          <Text style={styles.button}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.Purple,
+    flex: 1,
+    paddingTop: 20,
+    alignItems: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 10,
+    marginBottom: 50,
+    color: Colors.White,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    fontSize: 25,
+  },
+  buttonTouchable: {
+    borderRadius: 6,
+    backgroundColor: Colors.Yellow,
+    borderWidth: 2,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    marginTop: 15
+  },
+  input: {
+    backgroundColor: Colors.White,
+    fontSize: 20,
+    color: Colors.Black,
+    height: 60,
+    width: 300,
+    margin: 10,
+    padding: 10,
+  }
+});
+
+export default NewDeck;
